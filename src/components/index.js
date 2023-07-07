c3a2b0ac4aa2b4545ff7b02659fed3a576d28a88
cyberPh03nix <dowmohsen@gmail.com>
2023-07-07 18:32:17 +0330

    Initial commit

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import Navbar from "./Navbar.jsx";
+import Billing from "./Billing.jsx";
+import Business from "./Business.jsx";
+import Button from "./Button.jsx";
+import CardDeal from "./CardDeal.jsx";
+import Clients from "./Clients.jsx";
+import CTA from "./CTA.jsx";
+import Feedback from "./Feedback.jsx";
+import Footer from "./Footer.jsx";
+import GetStarted from "./GetStarted.jsx";
+import Hero from "./Hero.jsx";
+import Stats from "./Stats.jsx";
+import Testimonials from "./Testimonials.jsx";
+
+export {
+    Navbar,Billing,Business,Button,CardDeal,Clients,CTA,Stats,Footer,Feedback,Testimonials,Hero,GetStarted
+};
